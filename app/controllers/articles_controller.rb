253766973a49f5974573596b3a4cd0fe9f0518d3
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2019-10-16 19:55:20 +0900

    first day commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,21 @@
 class ArticlesController < ApplicationController
 
 def index
+  @articles = Article.all.order("created_at DESC")
+end
+
+def new
+  @article = Article.new
+end
+
+def create(article_params)
+  @article = Article.create(article_params)
+  redirect_to action: :index
+end
+
+private
+def article_params
+  params.permit(:title, :text, :image)
 end
 
 end
