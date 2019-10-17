b821f5f0ee86eb12da96363e698b21553284dc0d
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2019-10-17 17:57:58 +0900

    complete basic

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+before_action :move_to_index, except: :index
+
 def index
   @articles = Article.all.order("created_at DESC")
 end
@@ -8,14 +10,42 @@ def new
   @article = Article.new
 end
 
-def create(article_params)
+def create
   @article = Article.create(article_params)
   redirect_to action: :index
 end
 
+def destroy
+  article = Article.find(params[:id])
+  if current_user.id == article.user_id
+    article.destroy
+    redirect_to action: :index
+  end
+end
+
+def edit
+  @article = Article.find(params[:id])
+end
+
+def update
+  article = Article.find(params[:id])
+  if current_user.id == article.user_id
+    article.update(article_params)
+    redirect_to action: :index
+  end
+end
+
+def show
+end
+
 private
+
 def article_params
-  params.permit(:title, :text, :image)
+  params.require(:article).permit(:title, :text, :image).merge(user_id: current_user.id)
+end
+
+def move_to_index
+  redirect_to action: :index unless user_signed_in?
 end
 
 end
